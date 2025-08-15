99e1e3b94720ca96f4a12d8f19efcdbd472ab458
brunsssantos <brunasan7os@gmail.com>
2025-08-15 20:22:20 -0300

    Implementado CRUD de Produtos

---
+++ ControleDeBar.WebApp/Models/ProdutoViewModels.cs
@@ -0,0 +1,92 @@
+﻿using ControleDeBar.Dominio.ModuloGarcom;
+using ControleDeBar.Dominio.ModuloProduto;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeBar.WebApp.Models;
+
+public class CadastrarProdutoViewModel
+{
+    [Required(ErrorMessage = "O campo \"Nome\" é obrigatório.")]
+    [MinLength(2, ErrorMessage = "O campo \"Nome\" deve conter ao menos 2 caracteres.")]
+    public string Nome { get; set; }
+
+    [Range(1, 1000, ErrorMessage = "O valor do campo \"Valor\" precisa conter um valor entre 1 e 1000.")]
+    public decimal Valor { get; set; }
+
+    public CadastrarProdutoViewModel() 
+    {
+    }
+
+}
+
+public class EditarProdutoViewModel
+{
+    public int Id { get; set; } //Editar sempre tem o Id
+
+    [Required(ErrorMessage = "O campo \"Nome\" é obrigatório.")]
+    [MinLength(2, ErrorMessage = "O campo \"Nome\" deve conter ao menos 2 caracteres.")]
+    public string Nome { get; set; }
+
+    [Range(1, 1000, ErrorMessage = "O valor do campo \"Valor\" precisa conter um valor entre 1 e 1000.")]
+    public decimal Valor { get; set; }
+
+    public EditarProdutoViewModel()
+    {
+    }
+
+    public EditarProdutoViewModel(int id, string nome, decimal valor)
+    {
+        Id = id;
+        Nome = nome;
+        Valor = valor;
+    }
+}
+
+public class ExcluirProdutoViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public ExcluirProdutoViewModel()
+    {
+    }
+
+    public ExcluirProdutoViewModel(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
+public class VisualizarProdutosViewModel
+{
+    public List<DetalhesProdutoViewModel> Registros { get; set; } = new List<DetalhesProdutoViewModel>();
+
+    public VisualizarProdutosViewModel(List<Produto> produtos)
+    {
+        foreach ( Produto produto in produtos)
+        {
+            DetalhesProdutoViewModel detalhesVm = new DetalhesProdutoViewModel(
+                produto.Id,
+                produto.Nome,
+                produto.Valor
+            );
+
+            Registros.Add(detalhesVm);
+        }
+    }
+}
+
+public class DetalhesProdutoViewModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal Valor { get; set; }
+
+    public DetalhesProdutoViewModel(int id, string nome, decimal valor)
+    {
+        Id = id;
+        Nome = nome;
+        Valor = valor;
+    }
+}
